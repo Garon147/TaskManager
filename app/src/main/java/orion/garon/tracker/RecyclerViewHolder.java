78870311f56311bb4c2a1d9a78654cfd3764d539
Kirill Garon <garon147@gmail.com>
2017-05-08 22:30:42 +0300

    item layout

--- app/src/main/java/orion/garon/tracker/RecyclerViewHolder.java
+++ app/src/main/java/orion/garon/tracker/RecyclerViewHolder.java
@@ -2,6 +2,7 @@ package orion.garon.tracker;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -20,6 +21,12 @@ public class RecyclerViewHolder extends RecyclerView.ViewHolder {
     @Bind(R.id.state)
     public TextView state;
 
+    @Bind(R.id.dates)
+    public TextView dates;
+
+    @Bind(R.id.progress)
+    public ProgressBar progressBar;
+
     public RecyclerViewHolder(View itemView) {
         super(itemView);
         ButterKnife.bind(this, itemView);
@@ -29,6 +36,8 @@ public class RecyclerViewHolder extends RecyclerView.ViewHolder {
 
         title.setText(task.name);
         state.setText(task.state);
+        dates.setText(task.dueDate);
+        progressBar.setProgress(task.completion);
 
     }
 }
